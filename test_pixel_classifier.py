bf4aa8b1e7e8907bd03361ef4c9c92e421643333
Syed Rizvi <syedrizvi@Syeds-MacBook-Pro.local>
2022-02-03 14:40:53 -0600

    updated test pixel classifier script to just use one pixel classifier temporarily

--- test_pixel_classifier.py
+++ test_pixel_classifier.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 from random import seed
 from utils.utils import multi_acc, oht_to_scalar, colorize_mask
-from utils.data_util import tma_12_palette
+from utils.data_util import tma_12_palette, tma_12_class
 from torch.utils.data import DataLoader
 from pixel_features_dataset import PixelFeaturesDataset
 from networks.pixel_classifier import PixelClassifier
@@ -20,8 +20,14 @@ np.random.seed(chosen_seed)
 torch.manual_seed(chosen_seed)
 torch.cuda.manual_seed_all(chosen_seed)
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def log_string(logger, str1):
+    print(str1)
+    logger.write(str1 + "\n")
+    logger.flush()
 
 
 def plot_img_and_colored_mask(mask, ground_truth_img, image_num, SAVE_PATH, split):
@@ -38,11 +44,11 @@ def plot_img_and_colored_mask(mask, ground_truth_img, image_num, SAVE_PATH, spli
     colorize_ground_truth = colorize_mask(ground_truth_img, tma_12_palette)
 
     plt.imshow(colorized_mask)
-    plt.savefig(os.path.join(SAVE_PATH, split + "_image" + str(image_num) + "_ensemble_pred_mask.png"))
+    plt.savefig(os.path.join(SAVE_PATH, split + "_image" + str(image_num) + "_pred_mask.png"))
     plt.clf()
 
     plt.imshow(colorize_ground_truth)
-    plt.savefig(os.path.join(SAVE_PATH, split + "_image" + str(image_num) + "_ensemble_ground_truth_mask.png"))
+    plt.savefig(os.path.join(SAVE_PATH, split + "_image" + str(image_num) + "_ground_truth_mask.png"), bbox_inches='tight')
     plt.clf()
     plt.close()
 
@@ -68,6 +74,9 @@ def test_one_classifier(model_path, args, data_loader):
         ground_truth_mask = torch.zeros((1024, 1024))
         predicted_mask = torch.zeros((1024, 1024))
 
+        class_correct_count = [0 for _ in range(len(tma_12_class))]
+        class_total_count = [0 for _ in range(len(tma_12_class))]
+
         for idx, (data, ground_truth) in enumerate(data_loader):
             if idx % 100 == 0:
                 print("On batch idx", idx)
@@ -76,6 +85,12 @@ def test_one_classifier(model_path, args, data_loader):
 
             pred_logits = model(data)  # pred shape [b, 7]  # 7 class output probabilities
             pixel_preds = oht_to_scalar(pred_logits)
+
+            # Accumulating class-wise counts to compute class-wise accuracy later on
+            for i in range(len(pred_logits)):
+                class_total_count[ground_truth[i]] += 1
+                if ground_truth[i] == pixel_preds[i]:  # Correct prediction
+                    class_correct_count[ground_truth[i]] += 1
 
             ground_truth_mask[idx % 1024] = ground_truth
             predicted_mask[idx % 1024] = pixel_preds
@@ -99,46 +114,67 @@ def test_one_classifier(model_path, args, data_loader):
         ground_truth_mask_list = np.concatenate(ground_truth_mask_list, axis=0)  # Shape [num_images, 1024, 1024]
         predicted_mask_list = np.concatenate(predicted_mask_list, axis=0)
 
+    # Print out class-wise and total pixel-level accuracy
+    logger = open(os.path.join(SAVE_PATH, "test_accuracy_log.txt"), "w")
+    log_string(logger, "Class-wise Accuracies for Single Model:")
+    class_accuracies = []
+    for i in range(len(tma_12_class)):
+        if class_total_count[i] != 0:
+            class_accuracies.append(class_correct_count[i] / class_total_count[i])
+        else:
+            class_accuracies.append(-1)  # Not applicable, 0 pixels of this class in test set
+
+    for i in range(len(tma_12_class)):
+        log_string(logger, tma_12_class[i] + " Accuracy: " + str(round(class_accuracies[i], 5)))
+    log_string(logger, "")
+
+    log_string(logger, "Total Accuracy: " + str(round(sum(class_correct_count) / sum(class_total_count), 5)))
+    logger.close()
+
     return ground_truth_mask_list, predicted_mask_list
 
 
 def main():
-    training_set = PixelFeaturesDataset(args["pixel_feat_save_dir"], split="train")
-    validation_set = PixelFeaturesDataset(args["pixel_feat_save_dir"], split="val")
     test_set = PixelFeaturesDataset(args["pixel_feat_save_dir"], split="test")
-
-    # print("Length of test dataset: " + str(len(test_set)) + "\n")
-
-    train_dataloader = DataLoader(training_set, batch_size=1024, shuffle=False)
-    val_dataloader = DataLoader(validation_set, batch_size=1024, shuffle=False)
     test_dataloader = DataLoader(test_set, batch_size=1024, shuffle=False)
 
     split_name = "test"
-    all_classifiers_ground_truth_mask_list = []
-    all_classifiers_pred_mask_list = []
+    # all_classifiers_ground_truth_mask_list = []
+    # all_classifiers_pred_mask_list = []
+    #
+    # for i in range(args["model_num"]):
+    #     ground_truth_mask_list, predicted_mask_list = test_one_classifier(os.path.join(training_run_dir, training_run, "best_model_" + str(i) + ".pth"), args, test_dataloader)
+    #     all_classifiers_ground_truth_mask_list.append(np.expand_dims(ground_truth_mask_list, axis=0))
+    #     all_classifiers_pred_mask_list.append(np.expand_dims(predicted_mask_list, axis=0))
+    #
+    # all_classifiers_ground_truth_mask_list = np.concatenate(all_classifiers_ground_truth_mask_list, axis=0)  # [num_classifiers, num_images, 1024, 1024]
+    # all_classifiers_pred_mask_list = np.concatenate(all_classifiers_pred_mask_list, axis=0)
+    #
+    # # ModeResult is {mode, counts), and mode array is [1,nimgs,h,q] so do stats.mode(array, axis=0)[0][0]
+    # all_classifiers_ground_truth_mask_list = stats.mode(all_classifiers_ground_truth_mask_list, axis=0)[0][0]  # [num_images, 1024, 1024] majority voting mask
+    # all_classifiers_pred_mask_list = stats.mode(all_classifiers_pred_mask_list, axis=0)[0][0]
+    #
+    # for idx in range(len(all_classifiers_pred_mask_list)):
+    #     print("Saving predicted mask and ground truth mask for image", idx)
+    #     # all_classifiers_pred_mask_list[idx] is (1, 1, 1024, 1024)
+    #     plot_img_and_colored_mask(all_classifiers_pred_mask_list[idx], all_classifiers_ground_truth_mask_list[idx], idx, SAVE_PATH, split_name)
 
-    for i in range(args["model_num"]):
-        ground_truth_mask_list, predicted_mask_list = test_one_classifier(os.path.join(training_run_dir, training_run, "best_model_" + str(i) + ".pth"), args, test_dataloader)
-        all_classifiers_ground_truth_mask_list.append(np.expand_dims(ground_truth_mask_list, axis=0))
-        all_classifiers_pred_mask_list.append(np.expand_dims(predicted_mask_list, axis=0))
-
-    all_classifiers_ground_truth_mask_list = np.concatenate(all_classifiers_ground_truth_mask_list, axis=0)  # [num_classifiers, num_images, 1024, 1024]
-    all_classifiers_pred_mask_list = np.concatenate(all_classifiers_pred_mask_list, axis=0)
-
-    # ModeResult is {mode, counts), and mode array is [1,nimgs,h,q] so do stats.mode(array, axis=0)[0][0]
-    all_classifiers_ground_truth_mask_list = stats.mode(all_classifiers_ground_truth_mask_list, axis=0)[0][0]  # [num_images, 1024, 1024] majority voting mask
-    all_classifiers_pred_mask_list = stats.mode(all_classifiers_pred_mask_list, axis=0)[0][0]
-
-    for idx in range(len(all_classifiers_pred_mask_list)):
+    ground_truth_mask_list, predicted_mask_list = test_one_classifier(
+        os.path.join(training_run_dir, training_run, "best_model_" + str(0) + ".pth"),
+        args,
+        test_dataloader)
+    for idx in range(len(predicted_mask_list)):
         print("Saving predicted mask and ground truth mask for image", idx)
         # all_classifiers_pred_mask_list[idx] is (1, 1, 1024, 1024)
-        plot_img_and_colored_mask(all_classifiers_pred_mask_list[idx], all_classifiers_ground_truth_mask_list[idx], idx, SAVE_PATH, split_name)
+        plot_img_and_colored_mask(predicted_mask_list[idx], ground_truth_mask_list[idx], idx, SAVE_PATH, split_name)
 
 
 if __name__ == "__main__":
     training_run_dir = "/home/cougarnet.uh.edu/srizvi7/Desktop/Histopathology_Dataset_GAN/training-runs/"
-    training_run = "0011-TMA_Arteriole_stylegan2_ada"
-    SAVE_PATH = os.path.join(training_run_dir, training_run, "ensemble_mask_pred")
+    training_run = "0015-TMA_Arteriole_stylegan2_ada"
+    # training_run = "0013-pixel_classifier_saves"
+    # SAVE_PATH = os.path.join(training_run_dir, training_run, "ensemble_mask_pred")
+    SAVE_PATH = os.path.join(training_run_dir, training_run, "classifier0_mask_pred")
     if not os.path.exists(SAVE_PATH):
         os.mkdir(SAVE_PATH)
 
