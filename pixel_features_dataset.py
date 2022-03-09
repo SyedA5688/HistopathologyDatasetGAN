fixed dice score calculation, edited training set size, refactored scripts:

pixel_features_dataset.py
@@ -1,8 +1,12 @@
 import os
-import numpy as np
 
-from torch.utils.data import Dataset
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
 from utils.data_util import tma_4096_image_idxs
+
 
 class PixelFeaturesDataset(Dataset):
     def __init__(self, data_path, split="train", val_fold=0):
@@ -37,38 +41,42 @@ class PixelFeaturesDataset(Dataset):
         assert split in ["train", "val", "test"]
         assert val_fold in [0, 1, 2, 3, 4], "Unknown validation fold specified, must be 0-4"
 
+        train_val_split = 16
         if split == "train":
-            img_name_idxs = tma_4096_image_idxs[0:30]
+            img_name_idxs = tma_4096_image_idxs[0:train_val_split]
         elif split == "val":
-            start = 30 + 7 * val_fold
-            idxs = list(range(start, start + 7))
+            start = train_val_split + 4 * val_fold
+            idxs = list(range(start, start + 4))
             img_name_idxs = [tma_4096_image_idxs[i] for i in idxs]
         else:
-            start1 = 30 + 7 * val_fold
-            start2 = 30 + 7 * val_fold + 7
-            idxs = list(range(30, start1)) + list(range(start2, 65))
+            start1 = train_val_split + 4 * val_fold
+            start2 = train_val_split + 4 * val_fold + 4
+            idxs = list(range(train_val_split, start1)) + list(range(start2, len(tma_4096_image_idxs)))
             img_name_idxs = [tma_4096_image_idxs[i] for i in idxs]
 
         self.img_pixel_feat_len = 4096*4096
         self.split = split
         self.total_size = 0
-        self.class_samp_weights = {0: 0.0256, 1: 0., 2: 0.0192, 3: 0.1805, 4: 2.7027, 5: 0.3311}
+        self.pixel_feat_mean = np.load(os.path.join(data_path, "dataset_means.npy"))
+        self.pixel_feat_stds = np.load(os.path.join(data_path, "dataset_stds.npy"))
+        self.class_samp_weights = {0: 0.0258, 1: 0., 2: 0.0188, 3: 0.2299, 4: 2.7778, 5: 0.2994}
+        # ToDo: check if sampling weights are leading to correctly balanced batch
 
         self.features = {}
         for idx, image_idx in enumerate(img_name_idxs):
-            pixel_data = np.load(os.path.join(data_path, "pixel_features_dataset", "pixel_level_feat_img_{}.npy".format(image_idx)), mmap_mode='r')  # Shape (1048576, 6080)
-            self.features[idx] = pixel_data
+            pixel_data = np.load(os.path.join(data_path, "pixel_5block_features_dataset", "pixel_level_feat_img_{}.npy".format(image_idx)), mmap_mode='r')  # Shape (nsamples, nfeatures)
+            self.features[idx] = pixel_data  # ToDo: reduced pixels for faster iteration  [1024: 1024 * 3]
 
         self.ground_truth = {}
         for idx, image_idx in enumerate(img_name_idxs):
             mask = np.load(os.path.join(data_path, "image_{}_mask.npy".format(image_idx)))
             h, w = mask.shape
             image_pixel_label_list = []
-            for row in range(h):
+            for row in range(h):  # ToDo: reduced pixels by half for faster iteration  1024, 1024 * 3
                 for col in range(w):
-                    image_pixel_label_list.append(mask[row, col])  # Append 1 by 1, match order that pixel features were saved in
+                    image_pixel_label_list.append(mask[row, col])  # Append 1 by 1, ensure correct order
 
-            image_pixel_label_list = np.array(image_pixel_label_list)  # shape (1048576,)
+            image_pixel_label_list = np.array(image_pixel_label_list)
             self.total_size += len(image_pixel_label_list)
             self.ground_truth[idx] = image_pixel_label_list  # match pixel features
 
@@ -79,8 +87,24 @@ class PixelFeaturesDataset(Dataset):
         image_idx = index // self.img_pixel_feat_len
         pixel_feat_idx = index % self.img_pixel_feat_len
 
-        pixel_feat = self.features[image_idx][pixel_feat_idx]
         ground_truth_class = self.ground_truth[image_idx][pixel_feat_idx]
+        pixel_feat = self.features[image_idx][pixel_feat_idx].astype(np.float32)
+        pixel_feat = (pixel_feat - self.pixel_feat_mean) / self.pixel_feat_stds
 
         # returns shapes (6128,) and (1,)
         return pixel_feat, ground_truth_class
+
+
+if __name__ == "__main__":
+    from train_pixel_classifier import CustomWeightedRandomSampler
+
+    training_set = PixelFeaturesDataset(data_path="/data/syed/hdgan/TMA_4096_snapshot2600/", split="train")
+    sample_weights = [training_set.class_samp_weights[training_set.ground_truth[idx // training_set.img_pixel_feat_len][
+        idx % training_set.img_pixel_feat_len]] for idx in range(len(training_set))]
+    sampler = CustomWeightedRandomSampler(torch.DoubleTensor(sample_weights), len(training_set), replacement=True)
+
+    train_loader = DataLoader(training_set, batch_size=1048576, sampler=sampler)
+    # train_loader = DataLoader(training_set, batch_size=1048576, shuffle=True)
+    train_iter = iter(train_loader)
+    batch1 = train_iter.__next__()
+    print(batch1)

process_pixel_PCA_dataset_offline.py
@@ -12,7 +12,7 @@ from utils.utils import latent_to_image
 
 np.random.seed(0)
 torch.manual_seed(0)
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 # device = 'cuda' if torch.cuda.is_available() else 'cpu'
 device = 'cpu'
 
@@ -22,6 +22,12 @@ def check_img_idx_in_filename_tma4096(all_files, img_idx):
         if file == "image_{}.png".format(img_idx):
             return True
     return False
+
+
+def center_and_scale(pixel_feature_list):
+    pixel_feat_mean = np.mean(pixel_feature_list, axis=0, dtype=np.float64)
+    pixel_feat_std = np.std(pixel_feature_list, axis=0, dtype=np.float64)
+    return (pixel_feature_list - pixel_feat_mean) / pixel_feat_std
 
 
 def create_pixel_classifier_compressed_dataset(data_path, g_all, upsamplers, save_path):
@@ -34,9 +40,9 @@ def create_pixel_classifier_compressed_dataset(data_path, g_all, upsamplers, sav
     all_files = [file for file in all_files if ".png" in file]
     ipca = IncrementalPCA(n_components=100)
 
-    for img_name_idx in range(0, len(latent_np) + 1):
+    for img_name_idx in range(0, 38):  # len(latent_np) + 1
         if check_img_idx_in_filename_tma4096(all_files, img_name_idx):
-            print("Processing image", img_name_idx)
+            print("Fitting on image", img_name_idx)
             is_w_latent = img_name_idx == 0
             latent = torch.from_numpy(avg_latent_np).to(device).unsqueeze(0) if is_w_latent \
                 else torch.from_numpy(latent_np[img_name_idx - 1]).to(device).unsqueeze(0)
@@ -53,16 +59,14 @@ def create_pixel_classifier_compressed_dataset(data_path, g_all, upsamplers, sav
                     pixel_features_list.append(upsampled_featmap[:, :, row, col])  # Append [1, 6128]
 
             pixel_features_list = torch.cat(pixel_features_list, dim=0).numpy()  # [4096*4096, 6128]
-            pixel_feat_mean = np.mean(pixel_features_list, axis=0, dtype=np.float64)
-            pixel_feat_std = np.std(pixel_features_list, axis=0, dtype=np.float64)
-            pixel_feat_shifted_scaled = (pixel_features_list - pixel_feat_mean) / pixel_feat_std
+            pixel_features_list = center_and_scale(pixel_features_list)
 
             # Fit this chunk of the data. Calculating PCA incrementally
-            for feat_idx in range(0, len(pixel_feat_shifted_scaled), 1048576):
-                ipca.partial_fit(pixel_feat_shifted_scaled[feat_idx:feat_idx + 1048576])
+            for feat_idx in range(0, len(pixel_features_list), 262144):
+                ipca.partial_fit(pixel_features_list[feat_idx:feat_idx + 262144])
 
     print("\n\nFinished fitting PCA incrementally. Transforming and saving data now")
-    for img_name_idx in range(0, len(latent_np) + 1):
+    for img_name_idx in range(0, 92):  # len(latent_np) + 1
         if check_img_idx_in_filename_tma4096(all_files, img_name_idx):
             print("Processing image", img_name_idx)
             is_w_latent = img_name_idx == 0
@@ -82,8 +86,8 @@ def create_pixel_classifier_compressed_dataset(data_path, g_all, upsamplers, sav
 
             pixel_features_list = torch.cat(pixel_features_list, dim=0).numpy()  # [4096*4096, 6128]
             transformed_pca_features_list = []
-            for feat_idx in range(0, len(pixel_features_list), 1048576):
-                pixel_pca_features = ipca.transform(pixel_features_list[feat_idx:feat_idx + 1048576])
+            for feat_idx in range(0, len(pixel_features_list), 262144):
+                pixel_pca_features = ipca.transform(pixel_features_list[feat_idx:feat_idx + 262144])
                 transformed_pca_features_list.append(pixel_pca_features)
             transformed_pca_features = np.concatenate(transformed_pca_features_list, axis=0)
             np.save(os.path.join(save_path, "pixel_level_feat_PCA_img_{}.npy".format(img_name_idx)), transformed_pca_features)
@@ -108,8 +112,11 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--experiment', type=str, default="/home/cougarnet.uh.edu/srizvi7/Desktop/Histopathology_Dataset_GAN/experiments/TMA_4096_tile.json")
+    parser.add_argument('--experiment', type=str, default="/home/srizvi7/Desktop/Histopathology_Dataset_GAN/experiments/TMA_4096_tile.json")
+    # /home/cougarnet.uh.edu/srizvi7/Desktop/Histopathology_Dataset_GAN/experiments/TMA_4096_tile.json
     opts = parser.parse_args()
     args = json.load(open(opts.experiment, 'r'))
+    args["dataset_save_dir"] = "/project/hnguyen2/syed/hdgan/TMA_4096_snapshot2600"
+    args["stylegan_checkpoint"] = "/home/srizvi7/Desktop/Histopathology_Dataset_GAN/network-snapshot-002600.pkl"
 
     main()

test_pixel_classifier.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 
 from utils.utils import oht_to_scalar, colorize_mask, dice_coefficient
-from utils.data_util import tma_12_palette, tma_12_class
+from utils.data_util import tma_4096_crop_class, tma_4096_palette, tma_4096_image_idxs
 from pixel_features_dataset import PixelFeaturesDataset
 from networks.pixel_classifier import PixelClassifier
 
@@ -40,8 +40,8 @@ def plot_img_and_colored_mask(mask, ground_truth_img, image_num, split):
         split: dataset split images belong to
 
     """
-    colorized_mask = colorize_mask(mask, tma_12_palette)
-    colorize_ground_truth = colorize_mask(ground_truth_img, tma_12_palette)
+    colorized_mask = colorize_mask(mask, tma_4096_palette)
+    colorize_ground_truth = colorize_mask(ground_truth_img, tma_4096_palette)
 
     plt.imshow(colorized_mask)
     plt.savefig(os.path.join(SAVE_PATH, split + "_image" + str(image_num) + "_pred_mask.png"), bbox_inches='tight')
@@ -64,7 +64,7 @@ def test_one_classifier(model_path, data_loader):
     model.to(device)
     model.eval()
 
-    image_end_points = [1024 * i for i in range(1, 37)]
+    image_end_points = [4096 * i for i in range(1, 66)]
     ground_truth_mask_list = []
     predicted_mask_list = []
     dice_scores = []
@@ -73,16 +73,16 @@ def test_one_classifier(model_path, data_loader):
     image_counter = 0
 
     with torch.no_grad():
-        ground_truth_mask = torch.zeros((1024, 1024))
-        predicted_mask = torch.zeros((1024, 1024))
+        ground_truth_mask = torch.zeros((4096, 4096))
+        predicted_mask = torch.zeros((4096, 4096))
 
-        class_correct_count = [0 for _ in range(len(tma_12_class))]
-        class_total_count = [0 for _ in range(len(tma_12_class))]
+        class_correct_count = [0 for _ in range(len(tma_4096_crop_class))]
+        class_total_count = [0 for _ in range(len(tma_4096_crop_class))]
 
         for idx, (data, ground_truth) in enumerate(data_loader):
             if idx % 100 == 0:
                 print("On batch idx", idx)
-            data, ground_truth = data.to(device), ground_truth.long().to(device)
+            data, ground_truth = data.float().to(device), ground_truth.long().to(device)
 
             pred_logits = model(data)  # pred shape [b, 7]
             pixel_preds = oht_to_scalar(pred_logits)
@@ -93,8 +93,8 @@ def test_one_classifier(model_path, data_loader):
                 if ground_truth[i] == pixel_preds[i]:  # Correct prediction
                     class_correct_count[ground_truth[i]] += 1
 
-            ground_truth_mask[idx % 1024] = ground_truth
-            predicted_mask[idx % 1024] = pixel_preds
+            ground_truth_mask[idx % 4096] = ground_truth
+            predicted_mask[idx % 4096] = pixel_preds
 
             if (idx + 1) in image_end_points:
                 ground_truth_mask_one_hot = F.one_hot(ground_truth_mask.long(), num_classes=args["num_classes"])
@@ -111,23 +111,23 @@ def test_one_classifier(model_path, data_loader):
                 ground_truth_mask_list.append(np.expand_dims(np.copy(ground_truth_mask), axis=0))
                 predicted_mask_list.append(np.expand_dims(np.copy(predicted_mask), axis=0))
 
-                ground_truth_mask = torch.zeros((1024, 1024))
-                predicted_mask = torch.zeros((1024, 1024))
+                ground_truth_mask = torch.zeros((4096, 4096))
+                predicted_mask = torch.zeros((4096, 4096))
 
-        ground_truth_mask_list = np.concatenate(ground_truth_mask_list, axis=0)  # Shape [num_images, 1024, 1024]
+        ground_truth_mask_list = np.concatenate(ground_truth_mask_list, axis=0)  # Shape [num_images, 4096, 4096]
         predicted_mask_list = np.concatenate(predicted_mask_list, axis=0)
 
     # Print out class-wise and total pixel-level accuracy
     log_string(logger, "\n\nClass-wise Accuracies for Single Model:")
     class_accuracies = []
-    for i in range(len(tma_12_class)):
+    for i in range(len(tma_4096_crop_class)):
         if class_total_count[i] != 0:
             class_accuracies.append(class_correct_count[i] / class_total_count[i])
         else:
             class_accuracies.append(-1)  # Not applicable, 0 pixels of this class in test set
 
-    for i in range(len(tma_12_class)):
-        log_string(logger, tma_12_class[i] + " Accuracy: " + str(round(class_accuracies[i], 5)))
+    for i in range(len(tma_4096_crop_class)):
+        log_string(logger, tma_4096_crop_class[i] + " Accuracy: " + str(round(class_accuracies[i], 5)))
     log_string(logger, "")
 
     log_string(logger, "Total Accuracy: " + str(round(sum(class_correct_count) / sum(class_total_count), 5)))
@@ -138,30 +138,30 @@ def test_one_classifier(model_path, data_loader):
 
 def main():
     test_set = PixelFeaturesDataset(args["dataset_save_dir"], split="test")
-    test_dataloader = DataLoader(test_set, batch_size=1024, shuffle=False)
+    test_dataloader = DataLoader(test_set, batch_size=4096, shuffle=False, num_workers=8, pin_memory=True)
 
     # val_set = PixelFeaturesDataset(args["dataset_save_dir"], split="val")
-    # val_dataloader = DataLoader(val_set, batch_size=1024, shuffle=False)
+    # val_dataloader = DataLoader(val_set, batch_size=4096, shuffle=False)
 
     split_name = "test"
     ground_truth_mask_list, predicted_mask_list = test_one_classifier(
         os.path.join(training_run_dir, training_run, "best_model_" + str(0) + "_ep0.pth"), test_dataloader)
     for idx in range(len(predicted_mask_list)):
-        print("Saving predicted mask and ground truth mask for image", idx)
-        # all_classifiers_pred_mask_list[idx] is (1, 1, 1024, 1024)
-        plot_img_and_colored_mask(predicted_mask_list[idx], ground_truth_mask_list[idx], idx, split_name)
+        print("Saving predicted mask and ground truth mask for image", tma_4096_image_idxs[30 + idx])
+        # all_classifiers_pred_mask_list[idx] is (1, 1, 4096, 4096)
+        plot_img_and_colored_mask(predicted_mask_list[idx], ground_truth_mask_list[idx], tma_4096_image_idxs[30 + idx], split_name)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--experiment', type=str,
-                        default="/home/cougarnet.uh.edu/srizvi7/Desktop/Histopathology_Dataset_GAN/experiments/TMA_Arteriole_stylegan2_ada.json")
+                        default="/home/cougarnet.uh.edu/srizvi7/Desktop/Histopathology_Dataset_GAN/experiments/TMA_4096_tile.json")
 
     opts = parser.parse_args()
     args = json.load(open(opts.experiment, 'r'))
 
     training_run_dir = "/home/cougarnet.uh.edu/srizvi7/Desktop/Histopathology_Dataset_GAN/training-runs/"
-    training_run = "0032-TMA_Arteriole_stylegan2_ada"
+    training_run = "TMA_4096-0005"
     SAVE_PATH = os.path.join(training_run_dir, training_run, "classifier0_ep0_test_mask_pred")
     if not os.path.exists(SAVE_PATH):
         os.mkdir(SAVE_PATH)

train_pixel_classifier.py
@@ -19,6 +19,9 @@ from utils.data_util import tma_4096_crop_class
 from networks.pixel_classifier import PixelClassifier
 from pixel_features_dataset import PixelFeaturesDataset
 
+import warnings
+warnings.filterwarnings("ignore")
+
 
 chosen_seed = 0
 seed(chosen_seed)
@@ -68,7 +71,7 @@ def validation(model, model_num, val_loader, criterion, lowest_val_loss, highest
         dice_scores = []
 
         for batch_idx, (data, ground_truth) in enumerate(val_loader):
-            data, ground_truth = data.float().to(device), ground_truth.long().to(device)
+            data, ground_truth = data.to(device), ground_truth.long().to(device)
 
             pred_logits = model(data)
             loss = criterion(pred_logits, ground_truth)
@@ -92,7 +95,7 @@ def validation(model, model_num, val_loader, criterion, lowest_val_loss, highest
                 predicted_mask_one_hot = F.one_hot(predicted_mask.long(), num_classes=args["num_classes"])
 
                 dice_coeff = dice_coefficient(ground_truth_mask_one_hot, predicted_mask_one_hot)
-                dice_scores.append(dice_coeff)
+                dice_scores.append(dice_coeff.item())
 
                 ground_truth_mask = torch.zeros((args["featuremaps_dim"][0], args["featuremaps_dim"][1]))
                 predicted_mask = torch.zeros((args["featuremaps_dim"][0], args["featuremaps_dim"][1]))
@@ -104,8 +107,12 @@ def validation(model, model_num, val_loader, criterion, lowest_val_loss, highest
         val_avg_acc = summed_acc / (batch_idx + 1)
         val_avg_dice_coeff = mean(dice_scores)
 
-        if val_avg_acc > highest_val_acc:  # val_avg_loss < lowest_val_loss or
-            improved, improved_str = True, "(improved accuracy or val loss)"
+        tf_writer.add_scalar("Loss/val", val_avg_loss, epoch_num)
+        tf_writer.add_scalar("Accuracy/val", val_avg_acc, epoch_num)
+        tf_writer.add_scalar("Dice/val", val_avg_dice_coeff, epoch_num)
+
+        if val_avg_acc > highest_val_acc or val_avg_loss < lowest_val_loss or val_avg_dice_coeff > highest_val_dice:
+            improved, improved_str = True, "(improved)"
         else:
             improved, improved_str = False, ""
 
@@ -131,17 +138,12 @@ def validation(model, model_num, val_loader, criterion, lowest_val_loss, highest
         ####################################
         # Save model if there is improvement
         ####################################
-        if improved:
-            # Overwrite best saved model each time, only keep best model
-            save_name = "best_model_{}.pth".format(model_num) if epoch_num is None else "best_model_{}_ep{}.pth".format(model_num, epoch_num)
-            torch.save({
-                'model_state_dict': model.state_dict(),
-                'validation_loss': val_avg_loss
-            }, os.path.join(SAVE_PATH, save_name))
-
-        tf_writer.add_scalar("Loss/val", val_avg_loss, epoch_num)
-        tf_writer.add_scalar("Accuracy/val", val_avg_acc, epoch_num)
-        tf_writer.add_scalar("Dice/val", val_avg_dice_coeff, epoch_num)
+        # if improved:
+        save_name = "best_model_{}.pth".format(model_num) if epoch_num is None else "best_model_{}_ep{}.pth".format(model_num, epoch_num)
+        torch.save({
+            'model_state_dict': model.state_dict(),
+            'validation_loss': val_avg_loss
+        }, os.path.join(SAVE_PATH, save_name))
 
         return val_avg_loss, return_loss, return_acc, return_dice, improved
 
@@ -157,8 +159,8 @@ def train():
 
         classifier = nn.DataParallel(classifier).to(device)
         criterion = nn.CrossEntropyLoss()  # label_smoothing=0.5
-        # optimizer = optim.SGD(classifier.parameters(), lr=args["pixel_classifier_lr"])  # lr 0.05
-        optimizer = optim.Adam(classifier.parameters(), lr=args["pixel_classifier_lr"])  # lr 0.001
+        optimizer = optim.SGD(classifier.parameters(), lr=args["pixel_classifier_lr"], momentum=0.9)  # lr 0.05
+        # optimizer = optim.Adam(classifier.parameters(), lr=args["pixel_classifier_lr"])  # lr 0.001
 
         training_set = PixelFeaturesDataset(args["dataset_save_dir"], split="train")
         validation_set = PixelFeaturesDataset(args["dataset_save_dir"], split="val")
@@ -172,8 +174,9 @@ def train():
         sampler = CustomWeightedRandomSampler(torch.DoubleTensor(sample_weights), len(training_set), replacement=True)
 
         train_loader = DataLoader(training_set, batch_size=args['batch_size'], sampler=sampler, num_workers=16, pin_memory=True)
+        # ToDo: debug and check if batch is balanced, Arteriole pixels should be equal
         # train_loader = DataLoader(training_set, batch_size=args['batch_size'], shuffle=True, pin_memory=True)
-        val_loader = DataLoader(validation_set, batch_size=args['featuremaps_dim'][1], shuffle=False)
+        val_loader = DataLoader(validation_set, batch_size=args['featuremaps_dim'][1], shuffle=False, num_workers=32, pin_memory=True)
 
         # Training loop
         lowest_validation_loss = 10000000.
@@ -191,7 +194,7 @@ def train():
             for batch_idx, (data, ground_truth) in enumerate(train_loader):
                 # Move data and ground truth labels to cuda device, change ground truth labels to dtype long (integers)
                 # data is [b, 6128], ground_truth is [64,]
-                data, ground_truth = data.float().to(device), ground_truth.long().to(device)
+                data, ground_truth = data.to(device), ground_truth.long().to(device)
                 optimizer.zero_grad()
 
                 pred_logits = classifier(data)  # pred shape [b, 7]  # 7 class output probabilities